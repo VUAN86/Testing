6bd299385724630690f092072cb4983736040538
VUAN86 <vuan86@ya.ru>
2018-05-07 15:04:36 +0500

    Good Example

---
+++ TaskStore/src/java/store/controller/Message.java
@@ -0,0 +1,31 @@
+
+package store.controller;
+
+/**
+ *
+ * @author vukolov
+ */
+public class Message{
+        private String message;
+
+        public Message(String message) {
+            this.message = message;
+        }
+
+        public Message() {
+        }
+
+        public String getMessage() {
+            return message;
+        }
+
+        public void setMessage(String message) {
+            this.message = message;
+        }
+
+        @Override
+        public String toString() {
+            return "{\"message\":\"" + message + "\"}";
+        }
+        
+    }
